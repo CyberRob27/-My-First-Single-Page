60a9ad45f3d253f1846de81c20608597f0d281b9
CyberRob27 <43556150+CyberRob27@users.noreply.github.com>
2018-11-24 22:07:51 -0500

    Add files via upload

---
+++ Para.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Para extends Component {
+  render() {
+    return (
+      <div className="Para">
+    Rob Blog 1
+      </div>
+    );
+  }
+}
+
+export default Para;
